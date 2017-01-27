4c45a2d2bbe5fab8138f0ea88d911715b7860df4
Bryant <bryant.m.wang@gmail.com>
2017-01-27 09:15:35 -0800

    Add some starting data to firebase by way of the admin component, add code to the start component template to display a list of all members, add code to in the start component ts to pull the list from firebase.

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  styleUrls: ['./start.component.css'],
+  providers: [MemberService]
 })
 export class StartComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
